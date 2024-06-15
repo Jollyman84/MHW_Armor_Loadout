7e80dea9c6d1f2cc8b3529a6089daeab6d5b0696
Mr Jolly <greggoryantoine@gmail.com>
2024-06-15 13:58:37 -0400

    Added a swapArmor function.

--- armor_data.js
+++ armor_data.js
@@ -1,32 +1,60 @@
-document.onload = fetchDefault();
+// Loads default information upon page load 
+document.onload = [
+	fetchPartData(1,'head'),
+	fetchPartData(2,'torso'),
+	fetchPartData(3,'arms'),
+	fetchPartData(4,'belt'),
+	fetchPartData(5,'legs')
+];
 
-document.getElementById('swapHead').addEventListener('click', () => fetchPartData(11,'head'));
-document.getElementById('swapTorso').addEventListener('click', () => fetchPartData(12,'torso'));
-document.getElementById('swapArms').addEventListener('click', () => fetchPartData(13,'arms'));
-document.getElementById('swapBelt').addEventListener('click', () => fetchPartData(14,'belt'));
-document.getElementById('swapLegs').addEventListener('click', () => fetchPartData(15,'legs'));
+// Swaps picture and info for respective armor piece upon button click
+document.getElementById('swapHead').addEventListener('click', () => swapArmor('head'));
+document.getElementById('swapTorso').addEventListener('click', () => swapArmor('torso'));
+document.getElementById('swapArms').addEventListener('click', () => swapArmor('arms'));
+document.getElementById('swapBelt').addEventListener('click', () => swapArmor('belt'));
+document.getElementById('swapLegs').addEventListener('click', () => swapArmor('legs'));
 
-function fetchDefault() {
-	fetchPartData(1,'head');
-	fetchPartData(2,'torso');
-	fetchPartData(3,'arms');
-	fetchPartData(4,'belt');
-	fetchPartData(5,'legs');
-}
-
-// fetches data for a single armor piece
+// Fetches data for a single armor piece
 function fetchPartData(id, part) {
 	console.log('fetch');
 	fetch('https://mhw-db.com/armor/' + id.toString())
 		.then(response => response.json())
 		.then(armor => {
-			document.getElementById(part+'Img').src = armor['assets']['imageMale'];
-			document.getElementById(part+'Def').innerHTML = 'Defense: ' + armor['defense']['base'];
-			document.getElementById(part+'F').innerHTML = 'Fire: ' + armor['resistances']['fire'];
-			document.getElementById(part+'W').innerHTML = 'Water: ' + armor['resistances']['water'];
-			document.getElementById(part+'I').innerHTML = 'Ice: ' + armor['resistances']['ice'];
-			document.getElementById(part+'T').innerHTML = 'Thunder: ' + armor['resistances']['thunder'];
-			document.getElementById(part+'D').innerHTML = 'Dragon: ' + armor['resistances']['dragon'];
+			switch(armor['assets']) {
+				case null:
+				case undefined:
+					document.getElementById(part+'Img').src = './images/missing-image.png';
+					break;
+				default:
+					document.getElementById(part+'Img').src = armor['assets']['imageMale'];
+					break;
+			}
+			
+			document.getElementById(part+'Def').innerText = 'Defense: ' + armor['defense']['base'];
+			document.getElementById(part+'F').innerText = 'Fire: ' + armor['resistances']['fire'];
+			document.getElementById(part+'W').innerText = 'Water: ' + armor['resistances']['water'];
+			document.getElementById(part+'I').innerText = 'Ice: ' + armor['resistances']['ice'];
+			document.getElementById(part+'T').innerText = 'Thunder: ' + armor['resistances']['thunder'];
+			document.getElementById(part+'D').innerText = 'Dragon: ' + armor['resistances']['dragon'];
 		})
 		.catch((err) => console.error(err));
+}
+
+// TODO: Currently switches randomly, find an alternative
+// Switches a specific piece of armor
+function swapArmor(piece) {
+	fetch('./csv/' + piece + '_list.csv')
+		.then(response => response.text())
+		.then(response => response.split('\n'))
+		.then(response => response.map((x) => x.split(',')))
+		.then(armor => {
+			const index = Math.floor(Math.random()*armor.length);
+			console.log(armor[index][0]);
+			return armor[index][0];
+		})
+		.then(id => fetchPartData(id,piece))
+		.catch((err) => {
+			console.error(err);
+			return 1;
+		});
 }
